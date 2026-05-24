ebeff4b433a8c2eaf41d298151d0137a98a7596a
Kashmir Kudada <kashmirkudada60@gmail.com>
2026-05-24 10:07:14 +0530

    fizz buzz

---
+++ fizzBuzz.js
@@ -0,0 +1,12 @@
+const num = 1;
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else if (i % 3 == 0 && i % 5 == 0) {
+    console.log("Fizz Buzz");
+  } else {
+    console.log(i);
+  }
+}
