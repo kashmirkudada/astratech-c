1fb78e09336ed928335bc4b03060732a3c894621
Kashmir Kudada <kashmirkudada60@gmail.com>
2026-05-24 09:21:14 +0530

    if else

---
+++ if_else.js
@@ -0,0 +1,10 @@
+const age=32
+if(age< 18){
+    console.log("Not Approved")
+}else if(age>=18 && age<60){
+  console.log("Approved")
+}else if(age >60 && age<=150){
+  console.log("Not approved")
+}else{
+  console.log("You are not human")
+}
