97ee55736cedbcdadcf989c223fd765022558c1a
Kashmir Kudada <kashmirkudada60@gmail.com>
2026-05-29 10:04:33 +0530

    Minimum in array

--- MininArray.js
+++ MininArray.js
@@ -1,3 +1,4 @@
+//Find the minimum number in an array
 const arr = [12, 34, 45, 2];
 let min = arr[0];
 for (let i = 1; i < arr.length; i++) {
---
+++ object.js
@@ -0,0 +1,12 @@
+//object- group of key value pairs
+const student = {
+  name: "Aniket",
+  class: 12,
+  roll_no: 21,
+  marks: { eng: 10, maths: 98, sci: 75 },
+  points: [10, 30, 50, 100, 70, { a: 80 }],
+  age: 80,
+};
+console.log(student.marks.maths);
+console.log(student.points[4]);
+console.log(student.points[5].a);
