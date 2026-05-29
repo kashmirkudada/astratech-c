f829dcdef9925ffff910e0c48fc77d04ba879ba3
Kashmir Kudada <kashmirkudada60@gmail.com>
2026-05-29 09:04:27 +0530

    Sum of n number in array

---
+++ SumofArray.js
@@ -0,0 +1,7 @@
+//find the sum of n number of an array [16,12,13,24,17,2]
+const arr = [16, 12, 13, 24, 17, 2];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+console.log(sum);
